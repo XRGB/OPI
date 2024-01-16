84e16c3ba71d66374df738a0e2cb5f5ff69d8cef
vincentxrgb <vincent@xrgb.xyz>
2024-01-16 21:30:47 +0800

    fix length

--- modules/brc20_index/db_init.sql
+++ modules/brc20_index/db_init.sql
@@ -30,8 +30,8 @@ CREATE TABLE public.brc20_events (
 	event_type int4 NOT NULL,
 	block_height int4 NOT NULL,
 	inscription_id text NOT NULL,
-	from_address varchar(40) NOT NULL,
-	to_address varchar(40) NULL,
+	from_address varchar(255) NOT NULL,
+	to_address varchar(255) NULL,
 	"event" jsonb NOT NULL,
 	CONSTRAINT events_pk PRIMARY KEY (id)
 );
